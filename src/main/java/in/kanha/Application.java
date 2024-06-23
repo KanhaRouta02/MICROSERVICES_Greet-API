987ad5cb89ea44a866c927edd94c3e68768bf323
Kanha Routa <147900871+KanhaRouta02@users.noreply.github.com>
2024-06-23 21:35:17 +0530

    First Commit

--- src/main/java/in/kanha/Application.java
+++ src/main/java/in/kanha/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+// 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
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Application {
